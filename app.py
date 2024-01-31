2b53780765c5542e5a05c8db8460bd7e181dc247
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2024-01-31 10:23:34 +0100

    First commit

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FileField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_wtf.file import FileField, FileAllowed
 from forms import RegisterForm
 
 app = Flask(__name__)
+photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'images'  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/anasa/OneDrive/Bureau/twitter-clone/engage.db'
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'ksdlfkdsofidsithnaljnfadksjhfdskjfbnjewrhewuirhfsenfdsjkfhdksjhfdslfjasldkj'
 
+configure_uploads(app, photos)
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -41,16 +47,22 @@ def timeline():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        session = create_session()  # Create a session
+        session = create_session()
+
+        # Save the uploaded image
+        filename = photos.save(form.image.data)
+        image_url = photos.url(filename)
+
         new_user = User(
             name=form.name.data,
             username=form.username.data,
             password=form.password.data,
-            image=form.image.data  # Handle image upload here
+            image=image_url  
         )
         session.add(new_user)
         session.commit()
-        return redirect(url_for('index'))
+
+        return '<h1>Name: {}, Username: {}, Password: {}, Image URL: {}</h1>'.format(form.name.data, form.username.data, form.password.data, image_url)
     return render_template('register.html', form=form)
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, FileField
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired
 
 class RegisterForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    image = FileField('Profile Image')  # Add image field
+    image = FileField(validators=[FileAllowed(IMAGES, 'Only images are accepted.')])
     submit = SubmitField('Register')
