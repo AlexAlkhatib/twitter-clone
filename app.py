e5263dbc0ee3e560e5de4a42af4e9266ee9576a0
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2024-01-31 16:35:44 +0100

    Hash password

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_bcrypt import Bcrypt  # Import Flask-Bcrypt
 from forms import RegisterForm
 
 app = Flask(__name__)
 photos = UploadSet('photos', IMAGES)
+bcrypt = Bcrypt(app)  # Initialize Flask-Bcrypt
 
 app.config['UPLOADED_PHOTOS_DEST'] = 'images'  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/anasa/OneDrive/Bureau/twitter-clone/engage.db'
@@ -23,7 +25,7 @@ class User(db.Model):
     name = db.Column(db.String(100))
     username = db.Column(db.String(30))
     image = db.Column(db.String(100))  # Add column for image path
-    password = db.Column(db.String(50))
+    password = db.Column(db.String(100))  # Increase length for hashed password
 
 def create_session():
     engine = db.engine  # Access the engine within context
@@ -48,6 +50,9 @@ def register():
     if form.validate_on_submit():
         session = create_session()
 
+        # Hash the password
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+
         # Save the uploaded image
         filename = photos.save(form.image.data)
         image_url = url_for('uploaded_file', filename=filename)
@@ -55,7 +60,7 @@ def register():
         new_user = User(
             name=form.name.data,
             username=form.username.data,
-            password=form.password.data,
+            password=hashed_password,  # Store hashed password
             image=image_url  
         )
         session.add(new_user)
@@ -65,7 +70,7 @@ def register():
         return render_template('registration_result.html',
                                name=form.name.data,
                                username=form.username.data,
-                               password=form.password.data,
+                               password="Hashed Password",  # Display hashed password
                                image_url=image_url)
 
     return render_template('register.html', form=form)
