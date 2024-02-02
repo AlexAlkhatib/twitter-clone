1d8ce8ec117b568008880d152200b7891b80aa4c
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2024-02-02 07:16:03 +0100

    Make Profile General

--- app.py
+++ app.py
@@ -90,10 +90,22 @@ def login():
 
     return render_template('index.html', form=form)
 
-@app.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', current_user=current_user)
+@app.route('/profile', defaults={'username' : None})
+@app.route('/profile/<username>')
+def profile(username):
+
+    if username:
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            abort(404)
+    else:
+        user = current_user
+
+    tweets = Tweet.query.filter_by(user=user).order_by(Tweet.date_created.desc()).all()
+
+    current_time = datetime.now()
+
+    return render_template('profile.html', current_user=user, tweets=tweets, current_time=current_time)
 
 @app.route('/logout')
 @login_required
